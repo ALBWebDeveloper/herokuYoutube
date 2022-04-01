b30bc9f3ea5995035d24c4b6dff37c0332a2eedd
ALBWebDeveloper <97243758+ALBWebDeveloper@users.noreply.github.com>
2022-03-31 21:14:27 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req,res){
-    res.send("working app with node, gitHub, on Heroku server")
+    res.send("working app with node, gitHub, on Heroku server. How is it working now?")
 })
 
 app.listen(process.env.PORT||5000)
